aa2cc6e777123858d7f523ddf1d499c587843e7d
zhengxgs <35274397@qq.com>
2020-03-04 10:15:22 +0800

    test

--- rpc-startup/src/main/java/com/zhengxgs/proxy/RealSubject.java
+++ rpc-startup/src/main/java/com/zhengxgs/proxy/RealSubject.java
@@ -7,6 +7,7 @@ public class RealSubject implements Subject {
 
 	public String doSomething(String aaa, Integer bbb, int ccc) {
 		System.out.println("call doSomething()");
+		System.out.println("test");
 		return "123";
 
 	}
